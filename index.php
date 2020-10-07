3854cfcb5ad956c0aa91acd0686c090240d5f4fd
Gabriel Agrela <sopustos@gmail.com>
2020-10-07 21:09:21 +0100

    favicon update

--- index.php
+++ index.php
@@ -78,7 +78,8 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and $_POST['startWork']=="stop")
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Index</title>
+		<title>workLogs</title>
+		<link rel="icon" href="https://i.imgur.com/UDAqk7t.png">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
 		<style type="text/css">
 		body{ font: 14px sans-serif; }
