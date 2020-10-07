1e355ec1e151b362c13a031390a29e376267e0a2
Gabriel Agrela <sopustos@gmail.com>
2020-10-07 21:11:52 +0100

    favicon fix

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and $_POST['startWork']=="stop")
 	<head>
 		<meta charset="UTF-8">
 		<title>workLogs</title>
-		<link rel="icon" href="https://i.imgur.com/UDAqk7t.png">
+		<link rel="icon" type="image/png" href="https://i.imgur.com/UDAqk7t.png" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
 		<style type="text/css">
 		body{ font: 14px sans-serif; }
