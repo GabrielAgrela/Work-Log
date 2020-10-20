5b8c77093a576665492f57d5903917cf496998e3
Gabriel Agrela <sopustos@gmail.com>
2020-10-20 16:25:30 +0100

    fixed timezone

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('DB_SERVER', 'remotemysql.com');
 define('DB_USERNAME', '5omaMAZ0dZ');
 define('DB_PASSWORD', 'WHzYLTfC4I');
 define('DB_NAME', '5omaMAZ0dZ');
-
+date_default_timezone_set("europe/lisbon");
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
