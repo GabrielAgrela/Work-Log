f78cfecf6902848e75307c1a3043ee4dc327c1c7
Gabriel Agrela <sopustos@gmail.com>
2020-10-21 15:50:48 +0100

    favicon fix

--- index.php
+++ index.php
@@ -90,8 +90,8 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and $_POST['startWork']=="stop")
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="icon" type="image/png" href="media\favicon.png">
-</head>
+		<link rel="icon" type="image/png" href="media\favicon.png?">
+	</head>
 	<body>
 		<form action="logout.php">
 		    <input class="btn btn-danger" type="submit" value="LogOut" />
