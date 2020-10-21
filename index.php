8f7ae182f659b27006a1a2e427869a921c73b94b
Gabriel Agrela <sopustos@gmail.com>
2020-10-21 15:52:48 +0100

    favicon fix

--- index.php
+++ index.php
@@ -91,6 +91,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and $_POST['startWork']=="stop")
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<link rel="icon" type="image/png" href="media\favicon.png?">
+		<link rel="shortcut icon" type="image/png" href="media\favicon.png?">
 	</head>
 	<body>
 		<form action="logout.php">
