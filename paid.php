4eed64da169db006ec44bd6e34305a5962674a14
Gabriel Agrela <sopustos@gmail.com>
2021-03-04 22:48:38 +0000

    test mail

--- paid.php
+++ paid.php
@@ -11,32 +11,32 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
 }
 
 
-$mail = new PHPMailer(true);
+$to = '100cabessa@gmail.com';
+$subject = 'Aliens Abducted Me - Abduction Report';
+$msg = " was abducted  and was gone for.\n" .
+"Number of aliens: \n" .
+"Alien description: \n" .
+"What they did: \n" .
+"Fang spotted: \n" .
+"Other comments: ";
 
-//Send mail using gmail
-if($send_using_gmail){
-    $mail->IsSMTP(); // telling the class to use SMTP
-    $mail->SMTPAuth = true; // enable SMTP authentication
-    $mail->SMTPSecure = "ssl"; // sets the prefix to the servier
-    $mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
-    $mail->Port = 465; // set the SMTP port for the GMAIL server
-    $mail->Username = "ithrowthisaway1233321@gmail.com"; // GMAIL username
-    $mail->Password = "123456123456Aa"; // GMAIL password
-}
+$mail = new PHPMailer();
+$mail->  SMTPDebug=2;
+$mail ->  isSMTP();
+$mail ->  Host='smtp.gmail.com';
+$mail ->  SMTPAuth =true;
+$mail ->  Username='ithrowthisaway1233321@gmail.com';
+$mail ->  Password= '123456123456Aa';
+$mail ->  SMTPSecure ='tls';
+$mail ->  Port =587;
 
-//Typical mail data
-$mail->AddAddress("100cabessa@gmail.com", "jorge");
-$mail->SetFrom("100cabessa@gmail.com", "teste");
-$mail->Subject = "My Subject";
-$mail->Body = "Mail contents";
+$mail ->  setFrom($to);
+$mail ->  addAddress($to);
 
-try{
-    $mail->Send();
-    echo "Success!";
-} catch(Exception $e){
-    //Something went bad
-    echo "Fail - " . $mail->ErrorInfo;
-}
+$mail ->  addReplyTo($to);
+$mail ->  Subject=$subject;
+$mail ->  Body=$msg;
+$mail ->  send();
 /*$sql = "UPDATE worklog SET paid = 1 where id_user = ".$_SESSION['id'];
 if($stmt = mysqli_prepare($link, $sql))
 {
