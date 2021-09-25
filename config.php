594db8ede01ff33cc13f9c614e911f6bdf6e8b75
Gabriel Agrela <26043646+GabrielAgrela@users.noreply.github.com>
2021-09-25 23:17:17 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'remotemysql.com');
-define('DB_USERNAME', '5omaMAZ0dZ');
-define('DB_PASSWORD', 'WHzYLTfC4I');
-define('DB_NAME', '5omaMAZ0dZ');
+define('DB_SERVER', 'sql4.freesqldatabase.com');
+define('DB_USERNAME', 'sql4440065');
+define('DB_PASSWORD', '9ii7MMMfAN');
+define('DB_NAME', 'sql4440065');
 date_default_timezone_set("europe/lisbon");
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
