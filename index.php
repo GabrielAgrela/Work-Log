a79ffe55be950ecdfebacc6c6acc38259ccc7a84
Gabriel Agrela <26043646+GabrielAgrela@users.noreply.github.com>
2021-09-26 16:38:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -123,6 +123,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and $_POST['startWork']=="stop")
 						</tr>
 					</thead>
 		<?php
+					echo "SESSIONNNNNNNNNNNN2".$_SESSION["id"];
 			//select worklog data from this user
 			$sql = "SELECT id, description, start, finish, paid FROM worklog WHERE id_user = ?";
 			//for each row of data in worklog table, write a row in the html table
